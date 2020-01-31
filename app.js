e4b5db0aab893a5bfdb192a02e99970702a20e5e
satawatnack <60070093@kmitl.ac.th>
2020-01-31 14:15:37 +0700

    make it better

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 const express = require('express')
-const ngrok = require('ngrok');
-(async function() {
-  const url = await ngrok.connect(8080)
-  console.log(url)
-})();
 const app = express()
 const port = process.env.PORT || 8080
 app.post('/webhook', (req, res) => res.sendStatus(200))
