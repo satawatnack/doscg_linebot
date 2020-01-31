61ea6864f1c52580b36e403322812ec8e4a38e9a
satawatnack <60070093@kmitl.ac.th>
2020-01-31 15:08:41 +0700

    make it better

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.listen(port)
 function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {xxxxx}'
+        'Authorization': 'Bearer pGUvnj301pByYj93RZtj1pSVy1ROd82791zO1J677bjiJTnWoW2V9iZWdiTCd9FQ2v5NL0EE53+7KE5ZO4bYufglG1Uvu9xrOCqET6uLXYwAIqSyE5vU8zdtmc2e8YXs7kAoecME6juJFsBQox/fogdB04t89/1O/w1cDnyilFU='
     }
 
     let body = JSON.stringify({
