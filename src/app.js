c7ed2e2ea39f49db20bff30d3294fc3d78f65f24
satawatnack <60070093@kmitl.ac.th>
2020-02-01 10:58:02 +0700

    make it better

--- src/app.js
+++ src/app.js
@@ -8,13 +8,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
+    perf.start('apiCall')
+    await sleep(3000)
     reply(reply_token)
+    const results = perf.stop('apiCall')
+    if(results>=3000) replyagain(reply_token)
     res.sendStatus(200)
 })
 app.listen(port)
 async function reply(reply_token) {
-    perf.start('apiCall')
-    await sleep(3000)
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer pGUvnj301pByYj93RZtj1pSVy1ROd82791zO1J677bjiJTnWoW2V9iZWdiTCd9FQ2v5NL0EE53+7KE5ZO4bYufglG1Uvu9xrOCqET6uLXYwAIqSyE5vU8zdtmc2e8YXs7kAoecME6juJFsBQox/fogdB04t89/1O/w1cDnyilFU='
@@ -37,8 +39,6 @@ async function reply(reply_token) {
     }, (err, res, body) => {
         console.log('status = ' + res.statusCode)
     })
-    const results = perf.stop('apiCall')
-    if(results>=3000) replyagain(reply_token)
 }
 
 async function replyagain(reply_token) {
