7ed2365996dd92cc68bdb11825e3766808591f1b
satawatnack <60070093@kmitl.ac.th>
2020-02-01 10:53:45 +0700

    make it better

--- src/app.js
+++ src/app.js
@@ -38,7 +38,28 @@ async function reply(reply_token) {
         console.log('status = ' + res.statusCode)
     })
     const results = perf.stop('apiCall')
-    console.log(results.time)
+    if(results>=3000) replyagain(reply_token)
+}
+
+async function replyagain(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer pGUvnj301pByYj93RZtj1pSVy1ROd82791zO1J677bjiJTnWoW2V9iZWdiTCd9FQ2v5NL0EE53+7KE5ZO4bYufglG1Uvu9xrOCqET6uLXYwAIqSyE5vU8zdtmc2e8YXs7kAoecME6juJFsBQox/fogdB04t89/1O/w1cDnyilFU='
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'more than 10 seconds for ans'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode)
+    })
 }
 
 function sleep(millis) {
