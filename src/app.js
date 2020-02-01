e0b8c4b4d4a8dea77a20adf665dcd0a898618065
satawatnack <60070093@kmitl.ac.th>
2020-02-01 12:30:04 +0700

    make it better

--- src/app.js
+++ src/app.js
@@ -8,9 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    const time_check = reply(reply_token)
-    notify('Line Bot ' + time_check)
-    if (time_check>=10000) notify('Line Bot can not answer a question to the customer more than 10 second')
+    reply(reply_token)
     res.sendStatus(200)
 })
 app.listen(port)
@@ -40,7 +38,10 @@ async function reply(reply_token) {
         console.log('status = ' + res.statusCode)
     })
     const results = perf.stop('apiCall')
-    return results
+    notify('Line Bot' + results.time)
+    if (results.time>=10000) notify('Line Bot can not answer a question to the customer more than 10 second')
+    
+    
 }
 
 async function notify(text) {
